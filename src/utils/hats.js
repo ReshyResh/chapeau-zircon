4754418084509a9b957a5991836737e5ed157e9e
Reshy <reshy@protonmail.com>
2022-02-10 11:20:31 +0100

    Add hats objects

---
+++ src/utils/hats.js
@@ -0,0 +1,16 @@
+const hats = [
+    {
+        name: 'Red hat',
+        color: 'red'
+    },
+    {
+        name: 'Green hat',
+        color: 'green'
+    },
+    {
+        name: 'Blue hat',
+        color: 'blue'
+    }
+]
+
+export default hats;
