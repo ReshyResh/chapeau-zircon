5a69c50122ea13a42df611074c260041af65a87b
Reshy <reshy@protonmail.com>
2022-02-10 11:20:04 +0100

    Add tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/*.html', './src/*.jsx', './src/**/*.jsx', './src/**/*.html', './src/**/**/*.html', './src/**/**/*.jsx'],
+  theme: {},
+  variants: {
+    extend: {},
+  },
+};
